dd1f7fb49a0a5f1fe8b6f6c5ef28fbedf524013e
sosaaa123 <lautasosita097@gmail.com>
2025-10-28 20:13:04 -0300

    ddd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import "./App.css"
-import Stand from "./stand"
+import Stand from "./Stand"
+import Principal from "./Principal"
 function App() {
   return (
     <>
       <Routes>
         <Route path="/stand/:id" element={<Stand />}></Route>
+        <Route path="/" element={<Principal />}></Route>
       </Routes>
     </>
   )
---
+++ src/Principal.jsx
@@ -0,0 +1,17 @@
+export default function Principal() {
+  return (
+    <section className="principal">
+      <h1>Expotec 2025</h1>
+      <h2>Escuela de Educación Secundaria Técnica Nro1</h2>
+      <h3>Ing Jose Rafael Canton</h3>
+      <div className="redes">
+        <button>
+          <i class="fa-brands fa-facebook"></i>
+        </button>
+        <button>
+          <i class="fa-brands fa-instagram"></i>
+        </button>
+      </div>
+    </section>
+  )
+}
